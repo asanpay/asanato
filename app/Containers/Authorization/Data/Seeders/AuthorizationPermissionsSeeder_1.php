3b0a4de169b1d5f21887dccc58d552ff650b895c
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2020-05-20 00:59:59 +0430

    added missed permission

--- app/Containers/Authorization/Data/Seeders/AuthorizationPermissionsSeeder_1.php
+++ app/Containers/Authorization/Data/Seeders/AuthorizationPermissionsSeeder_1.php
@@ -23,6 +23,7 @@ class AuthorizationPermissionsSeeder_1 extends Seeder
         Apiato::call('Authorization@CreatePermissionTask', ['manage-admins-access', 'Assign users to Roles.']);
         Apiato::call('Authorization@CreatePermissionTask', ['access-dashboard', 'Access the admins dashboard.']);
 
+        Apiato::call('Authorization@CreatePermissionTask', ['search-users', 'Find user']);
         Apiato::call('Authorization@CreatePermissionTask', ['read-users', 'Read user data']);
         Apiato::call('Authorization@CreatePermissionTask', ['update-users', 'Update user profile']);
 
