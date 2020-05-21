df2b69500cad6f2c838c9164159ef2b817cebd54
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2020-05-21 20:49:38 +0430

    complted transfer_in_my_wallets doc

--- app/Containers/Transfer/UI/API/Routes/TransferBetweenMyWallets.v1.private.php
+++ app/Containers/Transfer/UI/API/Routes/TransferBetweenMyWallets.v1.private.php
@@ -1,28 +1,42 @@
 <?php
 
 /**
- * @apiGroup           Wallet
- * @apiName            CreateWallet
+ * @apiGroup           Transfer
+ * @apiName            TransferBetweenMyWallets
  *
- * @api                {POST} /v1/wallets CreateWallet
- * @apiDescription     Create a user wallet
+ * @api                {POST} /v1/transfer/my-wallets TransferBetweenMyWallets
+ * @apiDescription     Transfer Between My Wallets
  *
  * @apiVersion         1.0.0
- * @apiPermission      none
  *
- * @apiParam           {String}  name
- * @apiParam           {Boolean}  default
- * @apiParam           {String}  payer_wallet_id The wallet that should pay the wage
+ * @apiParam           {String}  src_wallet_id source wallet id
+ * @apiParam           {String}  dst_wallet_id destination wallet id
+ * @apiParam           {Int}  amount Transfer amount in Rial
+ * @apiParam           {String}  description (optional) transaction description
  *
  * @apiSuccessExample  {json}  Success-Response:
  * HTTP/1.1 200 OK
 {
-
+    "data": {
+        "object": "Tx",
+        "id": "l0865ybvrk73bgme",
+        "client_ip": "127.0.0.1",
+        "j_created_at": 13990301194349,
+        "created_at": "2020-05-21 19:43:49",
+        "tracking_id": 201416400001
+    },
+    "meta": {
+        "include": [],
+        "custom": []
+    },
+    "message": "everything's ok",
+    "code": 0,
+    "xTrackId": "8f03b26aef"
 }
  */
 
 /** @var Route $router */
-$router->post('transfer/transfer-between-my-wallets', [
+$router->post('transfer/my-wallets', [
     'as' => 'api_transfer_between_my_wallets',
     'uses'  => 'Controller@transferBetweenMyWallets',
     'middleware' => [
