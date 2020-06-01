6d1dfca341d312d47d13d38959872e3814adc382
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2020-06-01 11:15:14 +0430

    fixed users/find route doc

--- app/Containers/User/UI/API/Routes/FindUser.v1.private.php
+++ app/Containers/User/UI/API/Routes/FindUser.v1.private.php
@@ -10,7 +10,27 @@
  * @apiPermission      Authenticated User
  * @apiParam           {String}  keyword users mobile or email
  *
- * @apiUse             UserSuccessSingleResponse
+ * @apiSuccessExample  {json}       Success-Response:
+ *   HTTP/1.1 200 OK
+{
+    "data": {
+    "object": "User",
+        "id": "qmv7dk48x5b690wx",
+        "name": "کاربر آسان پی",
+        "email": "user@asanpay.com",
+        "mobile": "09354885725",
+        "avatar": "https://www.gravatar.com/avatar/50ef8d1422a08fa19c4c5c76da7d0577.jpg?s=128&d=mm&r=g"
+    },
+    "meta": {
+    "include": [
+        "roles"
+    ],
+        "custom": []
+    },
+    "message": "everything's ok",
+    "code": 0,
+    "xTrackId": "615fabda74"
+}
  */
 
 $router->get('users/find', [
