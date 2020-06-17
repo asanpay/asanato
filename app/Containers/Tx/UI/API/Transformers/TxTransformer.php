0927844c1abb69e0927fde99594492ff4fc51493
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2020-06-17 16:02:08 +0430

    updated TX transformer

--- app/Containers/Tx/UI/API/Transformers/TxTransformer.php
+++ app/Containers/Tx/UI/API/Transformers/TxTransformer.php
@@ -3,6 +3,7 @@
 namespace App\Containers\Tx\UI\API\Transformers;
 
 use App\Containers\Tx\Models\Tx;
+use App\Containers\Wallet\UI\API\Transformers\WalletTransformer;
 use App\Ship\Parents\Transformers\Transformer;
 
 class TxTransformer extends Transformer
@@ -18,7 +19,7 @@ class TxTransformer extends Transformer
      * @var  array
      */
     protected $availableIncludes = [
-
+        'wallet'
     ];
 
     /**
@@ -33,16 +34,31 @@ class TxTransformer extends Transformer
         $response = [
             'object'         => 'Tx',
             'id'             => $entity->getHashedKey(),
-            'client_ip'      => $meta->ip ?? 'unknown',
+            'wallet_id'      => $entity->wallet_id,
+            'type'           => $entity->type,
+            'transaction_id' => $entity->transaction_id,
+            'raw_amount'     => $entity->raw_amount,
+            'user_share'     => $entity->user_share,
+            'creditor'       => $entity->creditor,
+            'debtor'         => $entity->debtor,
+            'profit'         => $entity->profit,
+            'balance'        => $entity->balance,
+            'client_ip'      => $entity->ip_address ?? 'unknown',
             'j_created_at'   => $entity->j_created_at,
             'created_at'     => $entity->created_at,
             'tracking_id'    => $entity->tracking_id,
         ];
 
         $response = $this->ifAdmin([
-            'real_id'    => $entity->id
+            'real_id' => $entity->id,
         ], $response);
 
         return $response;
+    }
+
+    public function includeWallet(Tx $tx)
+    {
+        // use `item` with single relationship
+        return $this->item($tx->wallet, new WalletTransformer());
     }
 }
