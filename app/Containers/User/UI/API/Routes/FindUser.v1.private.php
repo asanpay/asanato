aa1229a3070c824a9fb5f12db0febc37f64ab0cf
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2020-12-29 10:58:39 +0330

    updated some documentation

--- app/Containers/User/UI/API/Routes/FindUser.v1.private.php
+++ app/Containers/User/UI/API/Routes/FindUser.v1.private.php
@@ -10,27 +10,7 @@
  * @apiPermission      Authenticated User
  * @apiParam           {String}  keyword users mobile or email
  *
- * @apiSuccessExample  {json}       Success-Response:
- *   HTTP/1.1 200 OK
-{
-    "data": {
-    "object": "User",
-        "id": "qmv7dk48x5b690wx",
-        "name": "کاربر آسان پی",
-        "email": "user@asanpay.com",
-        "mobile": "09354885725",
-        "avatar": "https://www.gravatar.com/avatar/50ef8d1422a08fa19c4c5c76da7d0577.jpg?s=128&d=mm&r=g"
-    },
-    "meta": {
-    "include": [
-        "roles"
-    ],
-        "custom": []
-    },
-    "message": "everything's ok",
-    "code": 0,
-    "xTrackId": "615fabda74"
-}
+ * @apiUse             UserSuccessSingleResponse
  */
 
 $router->get('users/find', [
--- app/Containers/User/UI/API/Routes/_user.v1.public.php
+++ app/Containers/User/UI/API/Routes/_user.v1.public.php
@@ -5,52 +5,22 @@
  * @apiSuccessExample {json} Success-Response:
 HTTP/1.1 200 OK
 {
-   "data":{
-      "object":"User",
-      "id":eqwja3vw94kzmxr0,
-      "name":"Mahmoud Zalt",
-      "email":"x.rolllln@hotmail.com",
-      "confirmed":"0",
-      "created_at":{
-         "date":"2017-06-06 05:40:51.000000",
-         "timezone_type":3,
-         "timezone":"UTC"
-      },
-      "updated_at":{
-         "date":"2017-06-06 05:40:51.000000",
-         "timezone_type":3,
-         "timezone":"UTC"
-      },
-      "readable_created_at":"1 second ago",
-      "readable_updated_at":"1 second ago",
-      "roles":{
-         "data":[
-            {
-               "object": "Role",
-               "id": abcderf,
-               "name":"admin",
-               "description":"Super Administrator",
-               "display_name":""
-            },
-            {
-               "object": "Role",
-               "id": ascderf,
-               "name":"client",
-               "description":"Special Client!",
-               "display_name":""
-            }
-         ]
-      }
-   },
-   "meta":{
-      "include":[
-         "stores",
-         "invoices",
-      ],
-      "custom":[
-
-      ]
-   }
+    "data": {
+    "object": "User",
+        "id": "qmv7dk48x5b690wx",
+        "name": "کاربر آسان پی",
+        "email": "user@asanpay.com",
+        "mobile": "09354885725",
+        "avatar": "https://www.gravatar.com/avatar/50ef8d1422a08fa19c4c5c76da7d0577.jpg?s=128&d=mm&r=g"
+    },
+    "meta": {
+    "include": [
+        "roles"
+    ],
+        "custom": []
+    },
+    "message": "everything's ok",
+    "code": 0,
+    "xTrackId": "615fabda74"
 }
- */
-
+*/
