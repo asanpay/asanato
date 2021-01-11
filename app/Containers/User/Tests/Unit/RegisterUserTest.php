e40ba79ca9d8f686883c2626ab4ddc5fbd491596
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2021-01-12 00:40:06 +0330

    handling new hostnames with sudo

--- app/Containers/User/Tests/Unit/RegisterUserTest.php
+++ app/Containers/User/Tests/Unit/RegisterUserTest.php
@@ -8,7 +8,6 @@ use App\Containers\Otp\Enum\OtpReason;
 use App\Containers\Otp\Tasks\GetLatestUnusedOtpTask;
 use App\Containers\User\Actions\UserSignUpAction;
 use App\Containers\User\Data\Transporters\UserSignUpTransporter;
-use App\Containers\User\Models\User;
 use App\Containers\User\Tests\TestCase;
 use Illuminate\Support\Facades\App;
 
